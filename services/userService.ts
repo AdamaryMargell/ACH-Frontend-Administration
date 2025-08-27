5967778358656762ee3008c406cc06b71d69af30
uchaniamum <uchaniamum@gmail.com>
2025-08-27 11:54:22 -0400

    AU/features change - users

--- services/userService.ts
+++ services/userService.ts
@@ -1,5 +1,5 @@
 import { API_CONFIG } from "~/config/api";
-import type { UserResponse } from "~/features/users/types";
+import type { UserDetailResponse, UserRequest, UserResponse, UserSaveResponse, UsersListResponse } from "~/features/users/types";
 
 class UserService {
     public baseURL = API_CONFIG.BASE_URL;
@@ -17,11 +17,11 @@ class UserService {
                     ...options.headers
                 }
             });
-            
+
             if (!response.ok) {
                 throw new Error(`Error: ${response.status} ${response.statusText}`);
             }
-            
+
             const data = await response.json();
             console.log(data);
             return data;
@@ -31,16 +31,46 @@ class UserService {
         }
     }
 
-    async getUsers(filters?: { search?: string }): Promise<UserResponse[]> {
+    async getUsers(filters?: { search?: string }): Promise<UsersListResponse[]> {
         let endpoint = 'users';
-        
-        if (filters?.search) {
-            const params = new URLSearchParams();
-            params.append('search', filters.search);
-            endpoint += `?${params.toString()}`;
-        }
 
-        return this.request<UserResponse[]>(endpoint);
+        if (filters?.search) {
+            const params = new URLSearchParams({ search: filters.search })
+            endpoint += `?${params.toString()}`
+        }
+        return this.request<UsersListResponse[]>(endpoint);
+    }
+
+    async getUserByCode(code: string): Promise<UserDetailResponse> {
+        if (!code) {
+            throw new Error('User code is required')
+        }
+        return this.request<UserDetailResponse>(`users/${code}`)
+    }
+
+    async createUser(userData: UserRequest): Promise<UserSaveResponse> {
+        console.log('Inserting user with data:', userData);
+        return this.request<UserResponse>('users', {
+            method: 'POST',
+            body: JSON.stringify(userData)
+        });
+    }
+
+    async updateUser(userData: UserRequest): Promise<UserSaveResponse> {
+        console.log('Updating user with data:', userData);
+        return this.request<UserSaveResponse>('users', {
+            method: 'PUT',
+            body: JSON.stringify(userData)
+        });
+    }
+
+    async resetPasswordUser(code: string): Promise<UserSaveResponse> {
+        if (!code) {
+            throw new Error('User code is required')
+        }
+        return this.request<UserSaveResponse>(`users/${code}/reset-mail`, {
+            method: 'PATCH'
+        });
     }
 }
 
